6f124c50f6d1f6137c8a40eb1c4c1f3040f34ae7
ghost9 <smuzairs@hotmail.com>
2020-01-30 16:32:42 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OldPhotoCopy copy = new OldPhotoCopy();
+            PhotoCopyInterface adapter = new TouchScreenAdapter(copy);
+
+            adapter.chooseFirstSelection();
+            adapter.chooseSecondSelection();
+            Console.ReadLine();
+        }
+    }
+}
